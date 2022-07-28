c7499104c297e473a72f7be9d1579a900b181558
locdt2 <locdtptit@gmail.com>
2022-07-28 15:13:21 +0700

    feat: update nx lastest version

--- apps/todos/jest.config.ts
+++ apps/todos/jest.config.ts
@@ -1,5 +1,6 @@
-module.exports = {
-  displayName: 'ui',
+/* eslint-disable */
+export default {
+  displayName: 'todos',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +9,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/libs/ui',
+  coverageDirectory: '../../coverage/apps/todos',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
